ae046ae9efda3367f18c36ba7c8f4bc53f33b60f
Jacob Junqi Tian <jacob@banana.abay.cf>
2022-07-22 20:38:08 +0000

    Implemented inference (embedding) scripts with data parallelism

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -5,16 +5,19 @@ from typing import Iterator, Dict
 import numpy as np
 import jax
 from flax.training.common_utils import shard
+from flax.jax_utils import replicate
 import datasets
+from transformers import FlaxRobertaModel
 
 from ..config import PipelineConfig, BatchKeys
 
 Dataset = datasets.arrow_dataset.Dataset
+Array = jax.numpy.ndarray
 
 
 def get_dataloader(
     dataset: Dataset, pipeline_args: PipelineConfig
-) -> Iterator[Dict[BatchKeys, jax.numpy.ndarray]]:
+) -> Iterator[Dict[BatchKeys, Array]]:
     """
     Return sharded fix-sized token batches from the dataset.
     Returns batches of identical sizes. Data that don't fit into a batch
@@ -32,3 +35,53 @@ def get_dataloader(
         batch = shard(batch)
 
         yield batch
+
+
+def _run_batch_inference_single_shard(
+    batch: Dict[BatchKeys, Array],
+    model: FlaxRobertaModel,
+    model_params: Dict,
+) -> Array:
+    """
+    Return the sentence-level embeddings for texts in this batch for one shard.
+
+    Args:
+     batch: Dict, from tokenizer.
+     model: HuggingFace sequence classification FLAX model.
+     model_params: parameters of the said model.
+
+    Returns:
+     ndarray of (batch_size, embedding_dimension).
+    """
+    outputs = model(**batch, params=model_params)
+    embeddings = outputs.pooler_output  # type: ignore
+    return embeddings
+
+
+_run_batch_inference_sharded = jax.pmap(
+    _run_batch_inference_single_shard, static_broadcasted_argnums=1
+)
+
+
+def run_batch_inference(
+    batch: Dict[BatchKeys, Array],
+    model: FlaxRobertaModel,
+) -> Array:
+    """
+    Return the sentence-level embeddings for texts in this batch.
+    This function runs in parallel across all accelerator cores.
+
+    Args:
+     batch: Dict, from tokenizer.
+     model: HuggingFace sequence classification FLAX model.
+     model_params: parameters of the said model.
+
+    Returns:
+     ndarray of (batch_size, embedding_dimension).
+    """
+    sharded_model_params = replicate(model.params)
+    sharded_embeddings: Array = _run_batch_inference_sharded(
+        batch, model, sharded_model_params
+    )
+    embeddings = sharded_embeddings.reshape((-1, sharded_embeddings.shape[-1]))
+    return embeddings
--- src/tests/test_predict_model.py
+++ src/tests/test_predict_model.py
@@ -2,10 +2,17 @@ import unittest
 
 import jax
 import chex
+from flax.jax_utils import replicate
 import datasets
 from datasets import load_from_disk
+from transformers import (
+    FlaxAutoModel,
+    AutoConfig,
+    RobertaConfig,
+    FlaxRobertaModel,
+)
 
-from ..models.predict_model import get_dataloader
+from ..models.predict_model import get_dataloader, run_batch_inference
 
 from ..config import PipelineConfig, ModelConfig, BatchKeys
 
@@ -16,6 +23,7 @@ num_devices = len(jax.devices())
 eval_per_device_batch_size = 2
 
 model_args = ModelConfig()
+pipeline_args = PipelineConfig(eval_per_device_batch_size=eval_per_device_batch_size)
 
 
 class GetDataLoaderFromProcessedDataset(unittest.TestCase):
@@ -25,18 +33,42 @@ class GetDataLoaderFromProcessedDataset(unittest.TestCase):
             self.assertIn(feature, self.preprocessed_dataset.features)  # type: ignore
 
     def test_batch_shape(self):
-        pipeline_args = PipelineConfig(
-            eval_per_device_batch_size=eval_per_device_batch_size
-        )
         dataloader = get_dataloader(self.preprocessed_dataset, pipeline_args)
 
         for batch in dataloader:
-            self.assertSetEqual(
-                set(batch.keys()), set(["attention_mask", "input_ids"])
-            )
+            self.assertSetEqual(set(batch.keys()), set(["attention_mask", "input_ids"]))
             for key in ["attention_mask", "input_ids"]:  # type: ignore
                 key: BatchKeys
                 value = batch[key]
                 chex.assert_axis_dimension(value, 0, jax.device_count())
                 chex.assert_axis_dimension(value, 1, eval_per_device_batch_size)
                 chex.assert_axis_dimension(value, 2, model_args.max_seq_length)
+
+
+class RunInferenceOnTextBatch(unittest.TestCase):
+    def setUp(self):
+        preprocessed_dataset: Dataset = load_from_disk(test_processed_dataset_path)  # type: ignore
+
+        self.dataloader = get_dataloader(preprocessed_dataset, pipeline_args)
+        self.model: FlaxRobertaModel = FlaxAutoModel.from_pretrained(
+            model_args.base_model_name
+        )
+        self.model_params = replicate(self.model.params)
+        self.hf_model_config: RobertaConfig = AutoConfig.from_pretrained(
+            model_args.base_model_name
+        )
+
+    def test_run_batch_inference_shape(self):
+        for batch in self.dataloader:
+            embeddings: jax.numpy.ndarray = run_batch_inference(batch, self.model)
+
+            self.assertEqual(
+                embeddings.shape[0],
+                num_devices * eval_per_device_batch_size,
+                "Number of outputs should match actual batch size.",
+            )
+            self.assertEqual(
+                embeddings.shape[-1],
+                self.hf_model_config.hidden_size,
+                "Embedding dim should match model specs.",
+            )
